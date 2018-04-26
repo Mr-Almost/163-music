6e37b4f9238544e8df682b088f548016ead36af3
Mr-Almost <499158415@qq.com>
2018-04-26 23:59:44 +0800

    a

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -1,37 +1,48 @@
 {
-    let view={
-        el:'.songList-container',
-        template:`
+    let view = {
+        el: '.songList-container',
+        template: `
         <ul class="songList">
       </ul>
         `,
-        render(data){
-            $(this.el).html(this.template)
-        },
-        clearActive(){
-            $(this.el).find('.active').removeClass('active')
-        }
+        render(data) {
+            let { songs } = data
+            let liList = songs.map((song)=> {
+        let li = $('<li></li>')
+        li.text(song.name)
+        return li
+    })
+    let $el=$(this.el)
+    $el.find('ul').emptyliList.map( (domLi)=>{
+        $el.find('ul'.append(domLi))
+    })
+    $(this.el).html(this.template)
+
+         },
+clearActive(){
+    $(this.el).find('.active').removeClass('active')
+ }
 
     }
-    let model={
-        data:[],
+let model = {
+    data: [],
 
-    }
-    let controller={
-        init(view,model){
-            this.view=view
-            this.model=model
+}
+let controller = {
+    init(view, model) {
+        this.view = view
+        this.model = model
+        this.view.render(this.model.data)
+        window.eventHub.on('upload', (data) => {
+            this.view.clearActive()
+        })
+        window.eventHub.on('create', () => {
+            this.model.data.push(data)
             this.view.render(this.model.data)
-            window.eventHub.on('upload',(data)=>{
-                this.view.clearActive()
-            })
-            window.eventHub.on('create',()=>{
-                this.model.data.push(data)
-                this.view.render(this.model.data)
-            })
-        }
+        })
     }
-    controller.init(view,model)
+}
+controller.init(view, model)
 
 
 
